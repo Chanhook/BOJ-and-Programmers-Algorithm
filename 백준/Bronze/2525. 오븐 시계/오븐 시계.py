f4662b926664c6e6489b5e72ae8c1e046e3d0d90
Kim Chanhyeok <76461625+Chanhook@users.noreply.github.com>
2022-05-11 22:51:13 +0900

    [Bronze IV] Title: 오븐 시계, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,21 @@
+import sys
+
+hh, mm = map(int, sys.stdin.readline().split())
+c = int(sys.stdin.readline().strip())
+
+sec = ((hh * 60 + mm) * 60) 
+new_sec = sec + c * 60
+
+
+new_min = new_sec // 60
+new_hh = new_min // 60
+new_hh %= 24
+new_mm = new_min % 60
+
+print(new_hh, new_mm)
+
+"""
+00 00
+23 59
+86340초
+"""
