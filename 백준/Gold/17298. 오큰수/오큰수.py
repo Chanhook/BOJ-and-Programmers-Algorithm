cd91def1cfb16700bc0bf799cfd0b0479f12b282
Kim Chanhyeok <76461625+Chanhook@users.noreply.github.com>
2022-06-30 10:25:47 +0900

    [Gold IV] Title: 오큰수, Time: 596 ms, Memory: 250256 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+A = list(map(int,sys.stdin.readline().split()))
+indices = [i for i in range(len(A))]
+
+answer = [-1 for _ in range(len(A))]
+stack=[indices[0]]
+
+for i in range(1,len(A)):
+    while stack and A[stack[-1]]<A[i]:
+        answer[stack.pop()]=A[i]
+    stack.append(i)
+
+print(*answer)
