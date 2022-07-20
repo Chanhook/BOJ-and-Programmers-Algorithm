8e9e19f190f34ac764c08570bb8b9a1e86519aa1
Kim Chanhyeok <76461625+Chanhook@users.noreply.github.com>
2022-07-20 09:40:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 4380 ms, Memory: 157192 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,30 +1,23 @@
 import sys
 sys.setrecursionlimit(10**9)
 
-# 노드의 개수
 n = int(sys.stdin.readline())
+tree = [[] for _ in range(n+1)]
+parents = [0 for _ in range(n+1)]
 
-# 트리
-Tree = [[] for _ in range(n+1)]
-# 부모 노드 저장
-Parents = [0 for _ in range(n+1)]
-# 트리 구조 입력
-for _ in range(n-1):
-    a, b = map(int, input().split())
-    Tree[a].append(b)
-    Tree[b].append(a)
-
-# print(Tree)
+for i in range(n-1):
+    n1,n2 = map(int,input().split())
+    tree[n1] += [n2]
+    tree[n2] += [n1]
 
 
-# 연결된 노드들부터 parents[i]의 부모가 없으면 부모 설정 해주고, DFS 돌린다.
-def DFS(start, tree, parents):
+def dfs(start,tree,parents):
     for i in tree[start]:
         if parents[i] == 0:
             parents[i] = start
-            DFS(i, tree, parents)
+            dfs(i,tree,parents)
 
-
-DFS(1, Tree, Parents)
-for p in Parents[2:]:
+dfs(1,tree,parents)
+for p in parents[2:]:
     print(p)
+    
