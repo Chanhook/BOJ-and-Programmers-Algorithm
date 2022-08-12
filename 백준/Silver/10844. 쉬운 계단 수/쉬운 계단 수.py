d0477d8c82cea4ecb603bb7423aaae7ad874f4ab
Kim Chanhyeok <76461625+Chanhook@users.noreply.github.com>
2022-08-12 17:23:35 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,18 +1,17 @@
 import sys
+mod = 1000000000
+n = int(sys.stdin.readline().strip())
 
-N = int(sys.stdin.readline().strip())
+dp = [[0]*10 for i in range(101)]
+dp[1] = [0]+[1]*9
 
-
-dp = [[0 for _ in range(10)] for __ in range(101)]
-dp[1]=[0,1,1,1,1,1,1,1,1,1]
-
-for i in range(2,101):
+for i in range(2, n+1):
     for j in range(10):
         if j == 0:
-            dp[i][j] = dp[i-1][1]
+            dp[i][j] = dp[i-1][j+1] 
         elif j == 9:
-            dp[i][j] = dp[i-1][8]
+            dp[i][j] = dp[i-1][j-1] 
         else:
             dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
 
-print(sum(dp[N])%1000000000)
+print(sum(dp[n])%mod)
