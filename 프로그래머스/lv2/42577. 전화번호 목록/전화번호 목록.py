7e0de67c56f6f02e6906120289f53328c2421c1b
Kim Chanhyeok <76461625+Chanhook@users.noreply.github.com>
2022-09-01 14:31:49 +0900

    [level 2] Title: 전화번호 목록, Time: 198.92 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,18 +1,11 @@
 def solution(phone_book):
-    # 정렬
-    phone_book.sort(key=len)
-    hash_table = {}
-
-    # 맨 처음 가장 짧은 길이를 저장. min.
-    min_len = len(phone_book[0])
-
-    for target in phone_book:
-        # loop마다 본인 정보를 hash table에 저장.
-        hash_table[hash(target)] = target
-        for i in range(min_len,len(target)):
-            try:
-                if hash_table[hash(target[:i])]:
-                    return False
-            except KeyError:
-                continue
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                return False
     return True
