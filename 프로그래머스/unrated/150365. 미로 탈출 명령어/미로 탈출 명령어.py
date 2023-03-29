c51f88bea497ddf4f692b75c74272beab5baa1e3
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-03-30 00:28:01 +0900

    [unrated] Title: 미로 탈출 명령어, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -0,0 +1,43 @@
+import sys
+sys.setrecursionlimit(100000)
+
+def solution(n, m, x, y, r, c, k):
+    def dfs(cur_x, cur_y, cur_path, cur_k):
+        if cur_x < 0 or cur_x >= n or cur_y < 0 or cur_y >= m:
+            return
+        
+        if flag[0] or abs(cur_x - (r-1)) + abs(cur_y - (c-1)) + cur_k > k:
+            return
+        
+        if cur_k == k:
+            if (cur_x, cur_y) == (r-1, c-1):
+                flag[0] = True
+                answer.append(''.join(cur_path)) 
+            return 
+        for i in range(4):
+            nx = cur_x + dx[i]
+            ny = cur_y + dy[i]
+            
+            cur_path.append(directions[i])
+            cur_k += 1
+            dfs(nx,ny,cur_path,cur_k)
+            cur_path.pop()
+            cur_k -= 1        
+    
+    z = k - abs(x - r) - abs(y - c)
+    if z < 0 or z % 2 != 0:
+        return 'impossible'
+    
+    answer = []
+    flag = [False]
+    
+    
+    dx = [1,0,0,-1]
+    dy = [0,-1,1,0]
+    directions = ['d','l','r','u']
+    
+    dfs(x-1, y-1, [], 0)
+    
+    if answer:
+        return answer[0]
+    return 'impossible'
