9167eefacb66763ee37de88c7b94592f666b4545
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-04-07 21:16:03 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 272 ms, Memory: 120432 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
@@ -1,64 +1,82 @@
-# 토네이도 회전 방향
-# 방향별 모래 비율 위치
-# a값과 격자 밖의 모래의 양
-def recount(x, y, direction):
-    if y < 0:
-        return
+def wind(cx, cy, cd):
+    sand = arr[cx][cy]
+    # 모든 모래가 이동 후 남은 모래 없음
+    arr[cx][cy] = 0
 
-    total = 0
-    for dx, dy, z in direction:
-        nx = x + dx
-        ny = y + dy
-        if z == 0:  # a
-            new_sand = sand[x][y] - total
-        else:
-            new_sand = int(sand[x][y] * z)
-            total += new_sand
+    # 흩어진 모래 총합
+    total_sand = 0
+    # 격자 밖으로 나간 모래
+    left = 0
 
-        if 0 <= nx < N and 0 <= ny < N:
-            sand[nx][ny] += new_sand
+    for i in range(10):
+        nx = cx + torX[cd][i]
+        ny = cy + torY[cd][i]
+
+        if i == 9:
+            # a에 들어갈 값
+            scattered = sand - total_sand
         else:
-            ans[0] += new_sand
-            
-    sand[x][y] = 0
+            scattered = int(sand * rate[i])
+            total_sand += scattered
+
+        if nx < 0 or nx >= N or ny < 0 or ny >= N:
+            left += scattered
+        else:
+            arr[nx][ny] += scattered
+
+    return left
+
+
+def solve():
+    ret = 0
+    cx = cy = N // 2
+
+    visited = [[0] * N for _ in range(N)]
+    cd = -1
+    while cx != 0 or cy != 0:
+        # 여기서 방문처리해줘야한다
+        visited[cx][cy] = 1
+        nd = (cd + 1) % 4
+
+        nx = cx + dx[nd]
+        ny = cy + dy[nd]
+
+        if visited[nx][ny]:
+            nd = cd
+            # 좌표도 바꿔줘야함
+            nx = cx + dx[nd]
+            ny = cy + dy[nd]
+
+        ret += wind(nx, ny, nd)
+
+        cx, cy, cd = nx, ny, nd
+
+    return ret
 
 
 N = int(input())
-sand = [list(map(int, input().split())) for _ in range(N)]
+arr = [list(map(int, input().split())) for _ in range(N)]
 
-left = [
-    (1, 1, 0.01),
-    (-1, 1, 0.01),
-    (1, 0, 0.07),
-    (-1, 0, 0.07),
-    (2, 0, 0.02),
-    (-2, 0, 0.02),
-    (1, -1, 0.1),
-    (-1, -1, 0.1),
-    (0, -2, 0.05),
-    (0, -1, 0),
+dx = (0, 1, 0, -1)
+dy = (-1, 0, 1, 0)
+
+torX = [
+    (1, -1, 2, -2, 0, 1, -1, 1, -1, 0),
+    (-1, -1, 0, 0, 2, 0, 0, 1, 1, 1),
+    (1, -1, 2, -2, 0, 1, -1, 1, -1, 0),
+    (1, 1, 0, 0, -2, 0, 0, -1, -1, -1)
 ]
-right = [(x, -y, z) for x, y, z in left]
-down = [(-y, x, z) for x, y, z in left]
-up = [(y, x, z) for x, y, z in left]
 
-s_x = s_y = N // 2
-ans = [0]
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
+torY = [
+    (1, 1, 0, 0, -2, 0, 0, -1, -1, -1),
+    (1, -1, 2, -2, 0, 1, -1, 1, -1, 0),
+    (-1, -1, 0, 0, 2, 0, 0, 1, 1, 1),
+    (1, -1, 2, -2, 0, 1, -1, 1, -1, 0)
+]
 
-# 토네이도 회전 방향
-tor_dir_dic = {0: left, 1: down, 2: right, 3: up}
-time = 0
+rate = [
+    0.01, 0.01, 0.02, 0.02, 0.05, 0.07, 0.07, 0.1, 0.1, 0
+]
 
-for i in range(2 * N - 1):
-    d = i % 4
-    if d == 0 or d == 2:
-        time += 1
-    for _ in range(time):
-        n_x = s_x + dx[d]
-        n_y = s_y + dy[d]
-        recount(n_x, n_y, tor_dir_dic[d])  # y좌표, 방향
-        s_x, s_y = n_x, n_y
-
-print(ans[0])
+ret = solve()
+print(ret)
