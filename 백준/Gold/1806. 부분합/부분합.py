2c0e450c5898082bfa04e1c921186361248eb866
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-08-14 01:53:54 +0900

    [Gold IV] Title: 부분합, Time: 160 ms, Memory: 125184 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+n, s = map(int, input().split())
+arr = list(map(int, input().split()))
+
+ans = n
+if sum(arr) < s:
+    print(0)
+else:
+    l, r = 0, 0
+    total = 0
+    while r < n:
+        total += arr[r]
+        if total < s:
+            r += 1
+        else:
+            while total >= s:
+                ans = min(r - l + 1, ans)
+                total -= arr[l]
+                l += 1
+            r += 1
+    print(ans)
