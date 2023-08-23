6acaa4e1bf817bd55a8c566d10eb010ef0baac2a
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-08-23 17:46:49 +0900

    [Silver V] Title: 배열 합치기, Time: 1168 ms, Memory: 337756 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -0,0 +1,7 @@
+import sys
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+print(*sorted(arr1 + arr2))
