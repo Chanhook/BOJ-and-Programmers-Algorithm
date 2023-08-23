35363570b7ae53d7d2bee1077869b6ff7565786e
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-08-23 17:19:47 +0900

    [Silver III] Title: 병든 나이트, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -0,0 +1,13 @@
+def solve():
+    n, m = map(int, input().split())
+    if n == 1:
+        return 1
+    elif n == 2:
+        return min(4, (m + 1) // 2)
+    elif m <= 6:
+        return min(4, m)
+    else:
+        return m - 2
+
+
+print(solve())
