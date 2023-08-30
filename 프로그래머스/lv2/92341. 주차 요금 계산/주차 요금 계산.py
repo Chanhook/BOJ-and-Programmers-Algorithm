84d211bc5dfb1ab4bf6d844c6093a074ceec30d1
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-08-30 15:36:48 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,63 +1,38 @@
 import math
 from collections import defaultdict
 
-def calculate_fee(fees, cumulate_time):
-    basic_time, basic_fee, unit_time, unit_fee = fees
-    
-    if cumulate_time > basic_time:
-        return basic_fee + math.ceil((cumulate_time - basic_time)/unit_time) * unit_fee
-    
-    return basic_fee
 
 def solution(fees, records):
     answer = []
-    
-    inout_dic = defaultdict(int)
-    car_time_dic = defaultdict(list)
-    
+    base_time, base_rate, unit_time, unit_rate = fees
+
+    park = set()
+    park_time = defaultdict(int)
+    cum_time = defaultdict(int)
+    last_time = 23 * 60 + 59
     for record in records:
-        infos = record.split()
-        h,m = map(int, infos[0].split(':'))
-        car_num = int(infos[1])
-        status = infos[2]
-        
-        if status == 'IN':
-            inout_dic[car_num] = 1
-            
-            if car_time_dic[car_num] == []:
-                car_time_dic[car_num].append(h*60 + m)
-            else:
-                car_time_dic[car_num][0] = h*60 + m
-                
+        time, car_num, history = record.split()
+        ho, mi = map(int, time.split(':'))
+        time = ho * 60 + mi
+
+        if car_num not in park:
+            park.add(car_num)
+            park_time[car_num] = time
         else:
-            if inout_dic[car_num] == 1:
-                inout_dic[car_num] = 0
-            
-            out_time = h*60 + m
-            in_time = car_time_dic[car_num][0]    
-            
-            if len(car_time_dic[car_num]) == 1:
-                car_time_dic[car_num].append(out_time - in_time)
-                car_time_dic[car_num][0] = 0
-            else:
-                car_time_dic[car_num][0] = 0
-                car_time_dic[car_num][1] += out_time-in_time
-        
-    for car_num, isin in inout_dic.items():
-        if isin == 1:
-            cur_time = car_time_dic[car_num][0]
-            out_time = 23*60 + 59
-            
-            if len(car_time_dic[car_num]) == 2:
-                car_time_dic[car_num][0] = 0
-                car_time_dic[car_num][1] += out_time-cur_time
-            else:
-                car_time_dic[car_num].append(out_time-cur_time)
-                car_time_dic[car_num][0] = 0
-    
-    sorted_car_time_list = sorted(car_time_dic.items(), key = lambda x: x[0])
-    
-    for car, times in sorted_car_time_list:
-        answer.append(calculate_fee(fees, times[1]))
-    
+            park.remove(car_num)
+            cum_time[car_num] += time - park_time[car_num]
+            park_time[car_num] = 0
+
+    while park:
+        car = park.pop()
+        cum_time[car] += last_time - park_time[car]
+
+    sorted_cum_time = sorted(cum_time.items(), key=lambda x: x[0])
+    for num, time in sorted_cum_time:
+        if time <= base_time:
+            answer.append(base_rate)
+        else:
+            pay = base_rate + math.ceil((time - base_time) / unit_time) * unit_rate
+            answer.append(pay)
+
     return answer
