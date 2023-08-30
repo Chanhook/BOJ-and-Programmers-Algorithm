fe2a3cfee190f10bd97bda0efe122bdd4d459841
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-08-30 14:16:48 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,35 +1,16 @@
-from collections import defaultdict
 def solution(id_list, report, k):
-    answer = []
-    
-    report_id_dic=defaultdict(set)
-    report_cnt_dic=defaultdict(int)
-    
-    send_dic=defaultdict(set)
-    
-    for string in report:
-        user, reported = string.split(" ")
-        l = len(report_id_dic[user])
-        report_id_dic[user].add(reported)
-        send_dic[reported].add(user)
-        if l != len(report_id_dic[user]):
-            report_cnt_dic[reported] += 1
-    
-    # print(report_id_dic)
-    # print(report_cnt_dic)
-    
-    # print(send_dic)
-    
-    answer_dic = defaultdict(int)
-    for user in id_list:
-        report_cnt = report_cnt_dic[user]
-        if report_cnt >= k:
-            for send_user in send_dic[user]:
-                answer_dic[send_user] += 1
-    
-    # print(answer_dic)
-    
-    for user in id_list:
-        answer.append(answer_dic[user])
-    
-    return answer
+    answer = {id: 0 for id in id_list}
+    reporting = {id: set() for id in id_list}
+    reported = {id: set() for id in id_list}
+
+    for r in report:
+        reporting_man, reported_man = r.split()
+        reporting[reporting_man].add(reported_man)
+        reported[reported_man].add(reporting_man)
+
+    for id in id_list:
+        if len(reported[id]) >= k:
+            for reporter in reported[id]:
+                answer[reporter] += 1
+
+    return [v for v in answer.values()]
