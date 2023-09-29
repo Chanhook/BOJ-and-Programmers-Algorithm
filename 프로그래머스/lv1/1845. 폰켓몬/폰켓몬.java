16024a0baf6eae2cf1b0c5157ae7187f737f1659
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-09-29 11:14:57 +0900

    [level 1] Title: 폰켓몬, Time: 2.17 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -2,18 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        
         Set<Integer> set = new HashSet<>();
-        int pick = nums.length / 2;
-        
-        for(int num : nums){
+        for (int num : nums) {
             set.add(num);
         }
-        
-        int setSize = set.size();
-        answer = Math.min(pick, setSize);
-        
-        return answer;
+
+        if (set.size() > nums.length / 2) {
+            return nums.length / 2;
+        } else {
+            return set.size();
+        }
     }
 }
