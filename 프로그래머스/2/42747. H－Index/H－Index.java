a995f8e5ef18831d7d7316379eee84b0d4611094
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-10-26 19:55:52 +0900

    [level 2] Title: H-Index, Time: 1.09 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int binarySearchLower(int target, int[] arr) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (target <= arr[mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+        for (int i = 10000; i > 0; i--) {
+            int idx = binarySearchLower(i, citations);
+            if (citations.length - idx >= i) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
