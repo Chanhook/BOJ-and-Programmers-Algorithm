2e60655b40180d7329493c66b863fd38cb2961fd
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-11-03 19:59:52 +0900

    [Gold IV] Title: 부분합, Time: 156 ms, Memory: 121716 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -4,17 +4,25 @@ input = sys.stdin.readline
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 
-ans = n
+cum = [0] * (n + 1)
+for i in range(n):
+    cum[i + 1] = cum[i] + arr[i]
+
 if sum(arr) < s:
     print(0)
 else:
-    l, r = 0, 0
-    total = 0
-    while r < n:
-        total += arr[r]
-        while total >= s:
-            ans = min(r - l + 1, ans)
-            total -= arr[l]
-            l += 1
-        r += 1
-    print(ans)
+    l = 0
+    r = 1
+    min_length = sys.maxsize
+    while l <= r:
+        cul_sum = cum[r] - cum[l]
+        while r < n and cul_sum < s:
+            r += 1
+            cul_sum = cum[r] - cum[l]
+
+        if cul_sum >= s:
+            length = r - l
+            min_length = min(length, min_length)
+        l += 1
+
+    print(min_length)
