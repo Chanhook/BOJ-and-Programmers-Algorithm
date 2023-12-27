440192b6e0a20bdadd8075fd5e769d2e6b438b51
Chanhyeok Kim <76461625+Chanhook@users.noreply.github.com>
2023-12-27 19:59:29 +0900

    [Gold V] Title: 빗물, Time: 148 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -9,41 +9,42 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int H = Integer.parseInt(st.nextToken());
         int W = Integer.parseInt(st.nextToken());
 
-        int[][] board = new int[H][W];
+        int[] heights = new int[W];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < W; i++) {
             int h = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < h; j++) {
-                board[H - j - 1][i] = 1;
-            }
+            heights[i] = h;
         }
 
         int total = 0;
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                if (board[i][j] == 1) {
-                    int count = 0;
-                    boolean flag = false;
-                    for (int k = j + 1; k < W; k++) {
-                        if (board[i][k] == 0) {
-                            count++;
-                        } else if (board[i][k] == 1) {
-                            flag = true;
-                            break;
-                        }
-                    }
-                    if (flag) {
-                        total += count;
-                    }
+        for (int i = 1; i < W - 1; i++) {
+            int curHeight = heights[i];
+
+            int leftMaxHeight = -1;
+            for (int j = 0; j < i; j++) {
+                if (heights[j] > leftMaxHeight) {
+                    leftMaxHeight = heights[j];
                 }
             }
-        }
 
+            int rightMaxHeight = -1;
+            for (int j = i + 1; j < W; j++) {
+                if (heights[j] > rightMaxHeight) {
+                    rightMaxHeight = heights[j];
+                }
+            }
+
+            int maxHeight = Math.min(leftMaxHeight, rightMaxHeight);
+            if (maxHeight > curHeight) {
+                total += maxHeight - curHeight;
+            }
+        }
         bw.write(total + "\n");
         bw.flush();
         br.close();
